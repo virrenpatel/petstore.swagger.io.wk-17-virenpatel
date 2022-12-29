406c3bde435c781501cee4f706730857f62a45d1
virrenpatel <virenpatel24611@gmail.com>
2022-12-29 19:30:18 +0000

    My homework-petstore.swagger.io-wk-17-virenpatel

---
+++ src/main/java/io/swagger/petstore/App.java
@@ -0,0 +1,13 @@
+package io.swagger.petstore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
